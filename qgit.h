5c0eabc3db8ef6a93a99af8a9a586befb86928f7
Boris Barbulovski <bbarbulovski@gmail.com>
2015-07-23 13:40:50 +0200

    Add binaries to ignore list

--- qgit.h
+++ qgit.h
@@ -1,7 +1,10 @@
 #ifndef QGIT_H
 #define QGIT_H
 
+#include "git2.h"
+
 #include <QObject>
+#include <QHash>
 #include <QDir>
 
 class QGit : public QObject
@@ -11,11 +14,12 @@ public:
     explicit QGit(QObject *parent = 0);
     virtual ~QGit();
 
-    void statusRepository(const QDir &path);
+public slots:
+    void repositoryStatus(QDir path);
 
 signals:
-
-public slots:
+    void repositoryStatusReply(QHash<git_status_t, int> items);
+    void error(QString qgit_function, QString git_function, int code);
 };
 
 #endif // QGIT_H
