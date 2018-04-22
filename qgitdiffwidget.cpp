7717b77019d4512fc505a2d99fa0220bc47fe2fe
Boris Barbulovski <bbarbulovski@gmail.com>
2018-04-22 21:57:32 +0200

    Initial git diff widget logic.

--- qgitdiffwidget.cpp
+++ qgitdiffwidget.cpp
@@ -37,17 +37,47 @@ void QGitDiffWidget::paintEvent(QPaintEvent *event)
 {
     QPainter painter(this);
 
+    int y = 0;
+
     for(int c = 0; c < m_fileRects.count(); c++)
     {
         QRect rect = m_fileRects.at(c);
 
         if (!event->region().intersected(QRegion(rect)).isEmpty())
         {
-            const QGitDiffFile &file = m_diff.at(c);
+            const auto file = m_diff.at(c);
+            const auto hunks = file.hunks();
 
             painter.fillRect(rect, QColor(220,220,220));
 
             painter.drawText(rect.left() + 10, rect.top() + 20, file.new_file().path());
+
+            for(int h = 0; h < hunks.count(); h++)
+            {
+                y += 12;
+
+
+                const auto hunk = hunks.at(h);
+                const auto lines = hunk.lines();
+
+                for(int l = 0; l < lines.count(); l++)
+                {
+                    QString old_lineNo, new_lineNo;
+                    const auto line = lines.at(l);
+
+                    if (line.old_lineno() >= 0) old_lineNo = QString::number(line.old_lineno());
+                    if (line.new_lineno() >= 0) new_lineNo = QString::number(line.new_lineno());
+
+                    painter.drawText(10, y, old_lineNo);
+                    painter.drawText(40, y, new_lineNo);
+
+                    painter.drawText(70, y, QString(QChar(line.origin())));
+
+                    painter.drawText(100, y, line.content());
+
+                    y += 13;
+                }
+            }
         }
     }
 }
