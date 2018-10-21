dccceaab06131199918aeeb62024503a8ea13362
Boris Barbulovski <bbarbulovski@gmail.com>
2018-10-21 23:59:20 +0200

    Update windows installer build script.

--- windows/build_installer.bat
+++ windows/build_installer.bat
@@ -1,9 +1,11 @@
 @echo off
 
 for /f "delims=" %%i in ('git rev-parse --short HEAD') do set git_hash=%%i
+for /f "delims=" %%i in ('git describe --tags --dirty') do set git_describe=%%i
 
 >installer.ini (
 	echo [installer]
+	echo target_name = "gitmaster_%git_describe%"
 	echo description = "Git GUI client(git hash #%git_hash%)"
 )
 
