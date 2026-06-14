6bec7a2091c75ebe71210b359fe9fe63f9fc728b
Boris Barbulovski <bbarbulovski@gmail.com>
2026-06-15 00:55:47 +0200

    Update build.bat

--- windows/build.bat
+++ windows/build.bat
@@ -3,7 +3,7 @@
 rmdir /s /q build
 
 call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-cmake.exe -S .. -B build -DCMAKE_BUILD_TYPE=Release -G Ninja
+cmake.exe -S .. -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=C:/Qt/6.12.0/msvc2022_64 -G Ninja
 cmake.exe --build build --config Release --verbose
 
 rmdir /s /q build
