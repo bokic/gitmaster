2b4ac8944e11f6eaf51ee1018d5a237498a1a304
Boris Barbulovski <bbarbulovski@gmail.com>
2026-08-15 14:18:14 +0200

    Add `#pragma once` to all headers.

--- combobox/qcomboboxgitdiffoptions.h
+++ combobox/qcomboboxgitdiffoptions.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "qcomboboxgitbase.h"
 
 class QComboBoxGitDiffOptions : public QComboBoxGitBase
--- combobox/qcomboboxgitviewoptions.h
+++ combobox/qcomboboxgitviewoptions.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "qcomboboxgitbase.h"
 
 class QComboBoxGitViewOptions : public QComboBoxGitBase
